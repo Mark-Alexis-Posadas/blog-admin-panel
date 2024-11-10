36243a58ead8e039e3fbc442597d6ec23fdc5777
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-11-10 09:01:35 +0800

    added pages and components

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const About: FC = () => {
+  return (
+    <section className="custom-min-h flex items-center justify-center">
+      <div className="mx-auto text-center">about</div>
+    </section>
+  );
+};
---
+++ client/src/pages/Blog.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Blog: FC = () => {
+  return (
+    <section className="custom-min-h flex items-center justify-center">
+      <div className="mx-auto text-center">Blog</div>
+    </section>
+  );
+};
