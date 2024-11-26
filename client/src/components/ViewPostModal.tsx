3afd58e8dd2f802d5da17df40b8fa9b6abf15348
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-11-26 18:14:40 +0800

    improve viewpost modal design

--- client/src/components/ViewPostModal.tsx
+++ client/src/components/ViewPostModal.tsx
@@ -20,7 +20,7 @@ export const ViewPostModal: FC<Type> = ({ viewPost, setIsToggleViewPost }) => {
 
   return (
     <div className="flex items-center justify-center fixed w-full top-0 left-0 min-h-screen bg-[rgba(0,0,0,0.4)]">
-      <div className="bg-white dark:bg-gray-700 p-5 md:p-10 w-[900px]">
+      <div className="bg-white dark:bg-gray-700  p-5 rounded w-[700px]">
         <div className="flex items-center justify-between mb-5">
           <h1 className="font-bold text-xl">Post</h1>
           <button onClick={() => setIsToggleViewPost(false)}>
@@ -28,8 +28,12 @@ export const ViewPostModal: FC<Type> = ({ viewPost, setIsToggleViewPost }) => {
           </button>
         </div>
         <ul>
-          <img src={viewPost.image} alt="" />
-          <li className="mb-3">
+          <img
+            src={viewPost.image}
+            alt={viewPost.image}
+            className="rounded w-full h-[400px] object-contain"
+          />
+          <li className="mt-5 mb-3">
             <strong>Title:</strong> {viewPost.title}
           </li>
           <li className="mb-3">
